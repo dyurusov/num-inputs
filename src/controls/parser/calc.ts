bb47292d3a7738f1478be631ee76ef58983b9381
Dmitry Yurusov <dyurusov@gmail.com>
2019-10-17 20:48:20 +0300

    make calc parser stub

--- src/controls/parser/calc.ts
+++ src/controls/parser/calc.ts
@@ -1,3 +1,15 @@
-import { NumericParser } from './numeric';
+import { ParserInterface, ValueType, ParsedType } from './types';
 
-export class CalcParser extends NumericParser {}
+export class CalcParser implements ParserInterface {
+  parse(value: ValueType): ParsedType {
+    if ((value === '') || (value === null) || (value === undefined)) {
+      return null;
+    }
+    const parsedValue: number = parseFloat(value as string);
+    return ((parsedValue === null) || (!isNaN(parsedValue) && (parsedValue.toString() === value.toString())))
+      ? parsedValue
+      : undefined;
+
+    // TODO: expression parsing
+  }
+}
