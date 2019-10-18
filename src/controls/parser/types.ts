3f5dc1e63e3bca58ac9632cefd51ea39c8415145
Dmitry Yurusov <dyurusov@gmail.com>
2019-10-18 11:18:47 +0300

    refactor numeric parser

--- src/controls/parser/types.ts
+++ src/controls/parser/types.ts
@@ -1,10 +1,5 @@
 export type ValueType = string | number | undefined | null;
 
-/**
- *  - number for correctly parsed value
- *  - null for empty parsed value
- *  - undefined for incorect value
- */
 export type ParsedType = number | undefined | null;
 
 export interface ParserInterface {
---
+++ src/controls/parser/utils/parse-numeric.ts
@@ -0,0 +1,39 @@
+import { ValueType, ParsedType } from '../types';
+
+/**
+ * @returns
+ *  - {number} for correctly parsed value
+ *  - {null} for empty parsed value
+ *  - {undefined} for incorect value
+ */
+export default function parseNumeric(value: ValueType): ParsedType {
+  // handle numbers and NaN
+  if (typeof value === 'number') {
+    return isNaN(value)
+      ? null
+      : (value || 0);  // convert -0 to 0
+  }
+
+  // handle undefined and null
+  if ((value === undefined) || (value === null)) {
+    return null;
+  }
+
+  // handle empty strings
+  const trimmed = value.trim();
+  if (!trimmed.length) {
+    return null;
+  }
+
+  // check for valid format and separate parts
+  const matches = trimmed.match(/^(\+|-)?(\d*)\.?(\d*)$/);
+  if (!matches || (!matches[2] && !matches[3])) {
+    return undefined;
+  }
+
+  const sign = (matches[1] === '-') ? -1 : 1;
+  const integralPart = matches[2] ? parseFloat(matches[2]) : 0;
+  const fractionalPart = matches[3] ? parseFloat(`0.${matches[3]}`) : 0;
+
+  return sign * (integralPart + fractionalPart) || 0; // convert -0 to 0
+}
